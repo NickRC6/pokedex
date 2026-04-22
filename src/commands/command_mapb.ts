de25b3910bb6493be55f86e7938630c5461c7961
NickRC6 <nickrc6@gmail.com>
2026-04-23 02:08:49 +0700

    Fix map output from full JSON to name string only

--- src/commands/command_mapb.ts
+++ src/commands/command_mapb.ts
@@ -4,6 +4,8 @@ export async function commandMapb(state: State): Promise<void> {
     const locations = await state.pokeAPI.fetchLocations(state.prevLocationsURL);
     state.prevLocationsURL = locations.previous
     state.nextLocationsURL = locations.next
-    console.log(locations)
+    for (const location of locations.results) {
+        console.log(location.name)
+    }
 }
 
