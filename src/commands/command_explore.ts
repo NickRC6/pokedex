0478d2f4e2b2598ae979f15e2ace6bdbc1872490
NickRC6 <nickrc6@gmail.com>
2026-04-24 21:02:14 +0700

    Typo fix in explore command

--- src/commands/command_explore.ts
+++ src/commands/command_explore.ts
@@ -2,7 +2,7 @@ import type { State } from "../state.js";
 
 export async function commandExplore(state: State, ...args: string[]) {
     if (args.length !== 1) {
-        throw new Error("you must provide a location name or ID");
+        throw new Error("You must provide a location name or ID");
     }
     const location = await state.pokeAPI.fetchLocation(args[0])
     console.log(location)
